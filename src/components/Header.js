669ab7e1a82e94b21bf190782d44cf19a871da73
edmcorrea <edm.correa@hotmail.com>
2022-08-22 16:00:39 -0300

    adding requirements 7, 8 e 9 - header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header({ pathname }) {
   const [nameHeader, setNameHeader] = useState('');
   const [showIcon, setshowIcon] = useState(true);
+  const [showSearchBar, setSearchBar] = useState(false);
   useEffect(() => {
     if (pathname === '/foods') {
       setNameHeader('Foods');
@@ -31,13 +34,27 @@ function Header({ pathname }) {
   }, [pathname]);
 
   return (
-    <>
-      <img src={ profileIcon } alt="profileIcon" data-testid="profile-top-btn" />
-      <p data-testid="page-title">{nameHeader}</p>
-      {showIcon && (
-        <img src={ searchIcon } alt="searchIcon" data-testid="search-top-btn" />
-      )}
-    </>
+    <div>
+      {(showSearchBar)
+        ? <SearchBar />
+        : (
+          <>
+            <Link to="/profile">
+              <img src={ profileIcon } alt="profileIcon" data-testid="profile-top-btn" />
+            </Link>
+            <p data-testid="page-title">{nameHeader}</p>
+            {showIcon && (
+              <button
+                type="button"
+                data-testid="search-top-btn"
+                onClick={ () => setSearchBar(true) }
+              >
+                <img src={ searchIcon } alt="searchIcon" />
+              </button>
+            )}
+          </>
+        )}
+    </div>
   );
 }
 
