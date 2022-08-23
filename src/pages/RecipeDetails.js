8d55750b87f08838d9921b5ef08067fe31940ca7
Pedro Papini <ppapinia@gmail.com>
2022-08-23 19:23:36 -0300

    Resolve requisitos 34 a 36 (menos testes)

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -4,7 +4,6 @@ import { useHistory } from 'react-router-dom';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
-import { singleMeal } from '../test/mocks/genericApiResponses';
 
 const copy = require('clipboard-copy');
 
@@ -13,12 +12,29 @@ const COPY_MESSAGE_TIMEOUT = 2000;
 function RecipeDetails() {
   const [showMessage, setShowMessage] = useState(false);
   const [isFavorite, setIsFavorite] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState({});
   const { location: { pathname } } = useHistory();
   const type = pathname.includes('foods') ? 'food' : 'drink';
-  const alcoholicOrNot = '';
+  const id = pathname.split('/')[2];
 
-  const { idMeal: id, strMeal: name, strArea: nationality,
-    strCategory: category, strMealThumb: image } = singleMeal.meals[0];
+  useEffect(() => {
+    const fetchData = async () => {
+      const endpoint = pathname.includes('foods')
+        ? `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+        : `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+      const objKey = pathname.includes('foods') ? 'meals' : 'drinks';
+      const request = await fetch(endpoint);
+      const data = await request.json();
+      console.log(data[objKey][0]);
+      setCurrentProduct(data[objKey][0]);
+    };
+    fetchData();
+  }, []);
+
+  const keyString = pathname.includes('foods') ? 'Meal' : 'Drink';
+  const { [`str${keyString}`]: name, strArea: nationality = '',
+    strCategory: category = '', [`str${keyString}Thumb`]: image,
+    strAlcoholic: alcoholicOrNot = '' } = currentProduct;
 
   useEffect(() => {
     const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
