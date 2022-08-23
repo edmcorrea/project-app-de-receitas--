5a8e6b127dacfea8920335f16b315d994a2c7c4e
Pedro Papini <ppapinia@gmail.com>
2022-08-23 20:50:37 -0300

    Adiciona possibilidade de passar um id para o componente FavoriteButton

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -3,20 +3,12 @@ import { useHistory } from 'react-router-dom';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
-function FavoriteButton({ currentProduct }) {
+function FavoriteButton({ currentProduct = {}, productId = undefined }) {
   const [isFavorite, setIsFavorite] = useState(false);
   const { location: { pathname } } = useHistory();
   const type = pathname.includes('foods') ? 'food' : 'drink';
   const keyString = pathname.includes('foods') ? 'Meal' : 'Drink';
-  const id = pathname.split('/')[2];
-
-  const {
-    [`str${keyString}`]: name,
-    strArea: nationality = '',
-    strCategory: category = '',
-    [`str${keyString}Thumb`]: image,
-    strAlcoholic: alcoholicOrNot = '',
-  } = currentProduct;
+  const id = productId ?? pathname.split('/')[2];
 
   useEffect(() => {
     const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
@@ -24,6 +16,13 @@ function FavoriteButton({ currentProduct }) {
   }, []);
 
   const addFavoriteRecipe = (favoriteRecipes) => {
+    const {
+      [`str${keyString}`]: name,
+      strArea: nationality = '',
+      strCategory: category = '',
+      [`str${keyString}Thumb`]: image,
+      strAlcoholic: alcoholicOrNot = '',
+    } = currentProduct;
     const currentRecipe = {
       id,
       type,
@@ -67,6 +66,7 @@ function FavoriteButton({ currentProduct }) {
 
 FavoriteButton.propTypes = {
   currentProduct: PropTypes.shape(PropTypes.shape).isRequired,
+  productId: PropTypes.string.isRequired,
 };
 
 export default FavoriteButton;
