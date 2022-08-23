503709b40ac17e3d062829af92490ba779024aa5
Danubio Rafael <danubio.rafaeel@gmail.com>
2022-08-23 17:57:14 -0300

    Req-24 done

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import { useRouteMatch } from 'react-router-dom';
+import fetchEndPoint from '../services/fetchFunction';
 
 function RecipeDetails() {
+  const [recipe, setRecipe] = useState({});
+  const { params: { idRecipe }, path } = useRouteMatch();
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      if (path.includes('foods')) {
+        const endPoint = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idRecipe}`;
+        const response = await fetchEndPoint(endPoint);
+        setRecipe(await response.meals);
+      } else {
+        const endPoint = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecipe}`;
+        const response = await fetchEndPoint(endPoint);
+        setRecipe((await response.drinks));
+      }
+    };
+    fetchRecipe();
+  },
+  []);
+
   return (
     <>
       <p> RecipeDetails </p>
