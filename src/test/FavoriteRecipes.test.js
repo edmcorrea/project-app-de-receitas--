43288847c672899396aef49facd930b60118381e
edmcorrea <edm.correa@hotmail.com>
2022-08-25 13:56:28 -0300

    adding merge projects

--- src/test/FavoriteRecipes.test.js
+++ src/test/FavoriteRecipes.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
+// import userEvent from '@testing-library/user-event';
+// import App from '../App';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
-import FavoriteRecipes from '../pages/FavoriteRecipes';
-import SearchBar from '../components/SearchBar';
+// import FavoriteRecipes from '../pages/FavoriteRecipes';
+// import SearchBar from '../components/SearchBar';
 import Recipes from '../pages/Recipes';
 
 describe('Verificação das rotas', () => {
