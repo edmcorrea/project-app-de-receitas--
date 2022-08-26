41c088179666dfd8ad467d6ce63da6fb4490495c
Pedro Papini <ppapinia@gmail.com>
2022-08-26 18:16:43 -0300

    Adiciona componente favorite button na tela de favoritos

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import FavoriteButton from '../components/FavoriteButton';
 import Header from '../components/Header';
 import ShareButton from '../components/ShareButton';
-import blackHeartIcon from '../images/blackHeartIcon.svg';
 import { nameHeader } from '../redux/actions';
 import '../styles/favoriteRecipes.css';
 
@@ -21,17 +21,6 @@ function FavoriteRecipes(props) {
     const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
     setStateFavoriteRecipes(favoriteRecipes);
   }, []);
-
-  const removeFavoriteRecipe = (favoriteRecipes, id) => {
-    const newFavoriteRecipes = favoriteRecipes.filter((recipe) => recipe.id !== id);
-    localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes));
-    setStateFavoriteRecipes(newFavoriteRecipes);
-  };
-
-  const handleFavoriteRecipeButton = (id) => {
-    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
-    removeFavoriteRecipe(favoriteRecipes, id);
-  };
 
   return (
     <div className="favorite-recipes">
@@ -68,49 +57,12 @@ function FavoriteRecipes(props) {
           Drinks
         </button>
       </section>
-      { filterAll ? (
-        stateFavoriteRecipes.map((fav, index) => (
-          <div key={ fav.id } className="card-favorite-recipe">
-            <Link
-              to={ `/${fav.type}s/${fav.id}` }
-            >
-              <img
-                src={ fav.image }
-                alt={ fav.name }
-                className="horizontal-image"
-                data-testid={ `${index}-horizontal-image` }
-              />
-              <div>
-                <p
-                  data-testid={ `${index}-horizontal-top-text` }
-                >
-                  {(fav.type === 'food')
-                    ? `${fav.nationality} - ${fav.category}`
-                    : fav.alcoholicOrNot}
-
-                </p>
-                <h3 data-testid={ `${index}-horizontal-name` }>{fav.name}</h3>
-              </div>
-            </Link>
-            <section>
-              <ShareButton path={ `${fav.type}s` } id={ fav.id } />
-              <button
-                type="button"
-                className="btn-type-meals"
-                data-testid={ `${index}-horizontal-favorite-btn` }
-                onClick={ () => handleFavoriteRecipeButton(fav.id) }
-                src={ blackHeartIcon }
-              >
-                <img
-                  src={ blackHeartIcon }
-                  alt="Favorite"
-                />
-              </button>
-            </section>
-          </div>
-        ))
-      ) : (
-        stateFavoriteRecipes.filter(({ type }) => type === typeFood).map((fav, index) => (
+      {stateFavoriteRecipes
+        .filter((recipe) => {
+          if (filterAll) return recipe;
+          return recipe.type === typeFood;
+        })
+        .map((fav, index) => (
           <div key={ fav.id } className="card-favorite-recipe">
             <Link to={ `/${fav.type}s/${fav.id}` }>
               <img
@@ -132,23 +84,10 @@ function FavoriteRecipes(props) {
             </Link>
             <section>
               <ShareButton path={ `${fav.type}s` } id={ fav.id } />
-              <button
-                type="button"
-                data-testid={ `${index}-horizontal-favorite-btn` }
-                onClick={ () => handleFavoriteRecipeButton(fav.id) }
-                src={ blackHeartIcon }
-                className="btn-type-meals"
-              >
-                <img
-                  src={ blackHeartIcon }
-                  alt="Favorite"
-                />
-              </button>
+              <FavoriteButton productId={ fav.id } />
             </section>
           </div>
-
-        ))
-      )}
+        ))}
     </div>
   );
 }
