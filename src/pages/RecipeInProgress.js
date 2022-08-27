52bcd7efe9403ec9af582f1e2461d27afc451596
Pedro Papini <ppapinia@gmail.com>
2022-08-26 23:15:16 -0300

    Estiliza receita em progresso

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
 import DetailCardTitle from '../components/DetailCardTitle';
+import Footer from '../components/Footer';
 import useGetRecipeForDetails from '../hooks/useGetRecipeForDetails';
 import '../styles/RecipeInProgress.css';
 
@@ -44,41 +45,51 @@ function RecipeinProgress() {
   };
 
   return (
-    <div className="detailCard">
+    <div className="recipe-in-progress">
 
       <DetailCardTitle recipe={ recipe } />
 
-      {ingredients.map((ingredient, index) => {
-        const ingredientString = (
-          `${ingredient[1]} ${measures[index] ? measures[index][1] : ''}`);
-        return (
-          <label
-            htmlFor={ ingredientString }
-            key={ `${ingredientString} ${index}` }
-            data-testid={ `${index}-ingredient-step` }
-            className="ingredient"
-          >
-            <input
-              id={ ingredientString }
-              type="checkbox"
-              checked={ usedIngredients.includes(ingredientString) }
-              onChange={ () => handleClickedIngredient(ingredientString) }
-            />
-            <span>{ingredientString}</span>
-          </label>
-        );
-      })}
+      <div className="recipe-text recipe-container">
+        <h3>Ingredients</h3>
+        <div className="ingredients-input">
+          {ingredients.map((ingredient, index) => {
+            const ingredientString = (
+              `${ingredient[1]} ${measures[index] ? measures[index][1] : ''}`);
+            return (
+              <label
+                htmlFor={ ingredientString }
+                key={ `${ingredientString} ${index}` }
+                data-testid={ `${index}-ingredient-step` }
+                className="ingredient"
+              >
+                <input
+                  id={ ingredientString }
+                  type="checkbox"
+                  checked={ usedIngredients.includes(ingredientString) }
+                  onChange={ () => handleClickedIngredient(ingredientString) }
+                />
+                <span className="ingredient-text">{ingredientString}</span>
+                <span className="checkmark" />
+              </label>
+            );
+          })}
+        </div>
+      </div>
 
-      <p data-testid="instructions">{recipe.strInstructions}</p>
-
+      <div className="recipe-text recipe-container">
+        <h3>Instructions</h3>
+        <p data-testid="instructions">{recipe.strInstructions}</p>
+      </div>
       <button
         type="button"
         onClick={ () => push('/done-recipes') }
         data-testid="finish-recipe-btn"
         disabled={ usedIngredients.length !== ingredients.length }
+        className="finishRecipe"
       >
         Finish recipe
       </button>
+      <Footer />
     </div>
   );
 }
